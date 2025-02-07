731c1eb378f1c6dff76d8672631d6230a593da62
Ben Gedi <bengedi@gmail.com>
2025-02-07 15:21:04 +0200

    add meal slug generation and XSS protection in saveMeal function

--- initdb.js
+++ initdb.js
@@ -169,16 +169,16 @@ const dummyMeals = [
 db.prepare(
   `
    CREATE TABLE IF NOT EXISTS meals (
-       id INTEGER PRIMARY KEY AUTOINCREMENT,
-       slug TEXT NOT NULL UNIQUE,
-       title TEXT NOT NULL,
-       image TEXT NOT NULL,
-       summary TEXT NOT NULL,
-       instructions TEXT NOT NULL,
-       creator TEXT NOT NULL,
-       creator_email TEXT NOT NULL
-    )
-`
+         id INTEGER PRIMARY KEY AUTOINCREMENT,
+         slug TEXT NOT NULL UNIQUE,
+         title TEXT NOT NULL,
+         image TEXT NOT NULL,
+         summary TEXT NOT NULL,
+         instructions TEXT NOT NULL,
+         creator TEXT NOT NULL,
+         creator_email TEXT NOT NULL
+      )
+   `
 ).run();
 
 async function initData() {
